937ab9ccd59c79513f88daf9edf15a779ff39b47
조현우 <hynoo20011229@gmail.com>
2025-09-25 18:29:49 +0900

    config 테스트 주소 추가

--- src/main/java/com/pixo/pixo_website/service/SecurityConfig.java
+++ src/main/java/com/pixo/pixo_website/service/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         //configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://29c78b5b87c1.ngrok-free.app"));
-        configuration.setAllowedOrigins(List.of("https://pixo-frontend.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://pixo-frontend.vercel.app", "https://pixo-frontend-git-frontendtest-hynoo20011229-4982s-projects.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-requested-with"));
         configuration.setAllowCredentials(true);
